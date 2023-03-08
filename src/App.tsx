72715a79801b889c42780a6d1ab1a377ae36914e
PG22Arseniy <pg22arseniy@vfs.com>
2023-03-07 17:37:25 -0800

    task interface, Submit Task event handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
-import React, {FC, useState, ChangeEvent} from 'react';
+import React, {FC, useState, ChangeEvent, MouseEvent} from 'react';
 import './App.css';
+import { ITask } from './Interfaces';
 
 const App: FC = () => {
 
   // App states
   const [task, setTask] = useState<string> ("")
   const [deadline, setDeadline] = useState<number> (0)
-  const [todo, setTodoList] = useState ([])
+  const [todoList, setTodoList] = useState<ITask[]> ([])
 
   // functions
-  const handleTaskChange = (e:ChangeEvent<HTMLInputElement>) => {
+  const handleTaskChange = (e:ChangeEvent<HTMLInputElement>): void => {
+    
     setTask (e.target.value)
+
   }
 
-  const handleDeadlineChange = (e:ChangeEvent<HTMLInputElement>) => {
+  const handleDeadlineChange = (e:ChangeEvent<HTMLInputElement>): void => {
+   
+    // convert input value to number
     const deadline:number = Number (e.target.value)
 
     setDeadline(deadline); 
+  }
+
+  const handleTaskSubmit = (e:MouseEvent<HTMLButtonElement>): void => {
+
+    const newTask: ITask = {
+      taskName: task,
+      deadline: deadline,
+      isDone: false  
+    } 
+    setTodoList([...todoList, newTask ]) 
   }
 
   return (
@@ -26,7 +41,7 @@ const App: FC = () => {
           <input onChange={handleTaskChange} name = "task" type ="text" placeholder="Task..." />
           <input onChange={handleDeadlineChange} name = "deadline" type ="number" placeholder="Expires in ... days" />
         </div>
-        <button> AddTask </button>  
+        <button onClick={handleTaskSubmit} name="taskSubmitBtn"> AddTask </button>  
       </div>
 
       <div className="todoList">
