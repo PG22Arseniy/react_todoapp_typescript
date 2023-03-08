f80555f1bd97aedb88f93e5081c4cd32f4406a5e
PG22Arseniy <pg22arseniy@vfs.com>
2023-03-07 17:24:56 -0800

    UseStates for task , taklist and Deadline, HandleChange Functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React, {FC} from 'react';
+import React, {FC, useState, ChangeEvent} from 'react';
 import './App.css';
 
 const App: FC = () => {
+
+  // App states
+  const [task, setTask] = useState<string> ("")
+  const [deadline, setDeadline] = useState<number> (0)
+  const [todo, setTodoList] = useState ([])
+
+  // functions
+  const handleTaskChange = (e:ChangeEvent<HTMLInputElement>) => {
+    setTask (e.target.value)
+  }
+
+  const handleDeadlineChange = (e:ChangeEvent<HTMLInputElement>) => {
+    const deadline:number = Number (e.target.value)
+
+    setDeadline(deadline); 
+  }
+
   return (
     <div className="App">
       <div className = "header">
         <div className='inputContainer'>
-          <input type ="text" placeholder="Task..." />
-          <input type ="number" placeholder="Expires in ... days" />
+          <input onChange={handleTaskChange} name = "task" type ="text" placeholder="Task..." />
+          <input onChange={handleDeadlineChange} name = "deadline" type ="number" placeholder="Expires in ... days" />
         </div>
         <button> AddTask </button>  
       </div>
