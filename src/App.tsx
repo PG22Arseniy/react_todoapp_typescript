a62c56a62ea6cd82d8ff9413611237af83c00cde
PG22Arseniy <pg22arseniy@vfs.com>
2023-03-07 16:50:32 -0800

    main html elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './App.css';
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
+      <div className = "header">
+        <input type ="text" placeholder="Task..." />
+        <input type ="number" placeholder="Expires in ... days" />
+        <button> AddTask </button> 
+      </div>
 
+      <div className="todoList">
+
+      </div>
     </div>
   );
 }
